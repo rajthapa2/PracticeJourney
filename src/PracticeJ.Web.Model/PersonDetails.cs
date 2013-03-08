38967b16db1aefd33bb9ec28fa5c2f94030ed087
unknown <RThapa@PEG-PC-005624.insurance.financial.local>
2013-03-08 09:27:43 +0000

    <addition of model files>

---
+++ src/PracticeJ.Web.Model/PersonDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticeJ.Web.Model
+{
+    [Serializable]
+    public class PersonDetails
+    {
+        public const string FirstNameRegex = @"^([a-z A-Z]|-|\u0027){2,15}$";
+        public const string LastNameRegex = @"^([a-z A-Z]|-|\u0027){2,20}$";
+
+
+        [Required]
+        [RegularExpression(Constants.ReferenceDataCodeFormat)]
+        public string TitleCode { get; set; }
+        
+        [Required]
+        [RegularExpression(FirstNameRegex)]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string DateOfBirth { get; set; }  
+
+    }
+}
